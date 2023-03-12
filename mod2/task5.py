b9d264143dbf1f62cd7de8007232d3113c556404
phantomfeed <doroh.danil@mail.ru>
2023-03-12 21:07:21 +0500

    mod2

--- mod2/task5.py
+++ mod2/task5.py
@@ -5,16 +5,9 @@ app = Flask(__name__)
 
 @app.route('/max_number/<path:numbers>')
 def max_number(numbers):
-    numbers = numbers.split('/')
-    dic = []
-    for num in numbers:
-        if num.isdecimal():
-            dic.append(int(num))
-        else:
-            return 'В URL передано не число'
-    num = max(dic)
-    return f'Максимальное число: <i>{num}</i>'
+    numbers_as_num = (int(it) for it in numbers.split('/'))
+    return f'Максимальное число <i>{max(numbers_as_num)}</i>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
