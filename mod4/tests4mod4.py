2fe327ca584f70f740ef823022a5e3ffbae2d120
phantomfeed <doroh.danil@mail.ru>
2023-03-20 01:43:35 +0500

    mod4

--- mod4/tests4mod4.py
+++ mod4/tests4mod4.py
@@ -70,6 +70,12 @@ class TestRegistrationForm(unittest.TestCase):
                                                       'comment': 'kwarg'})
         assert response.status_code == 400
 
+    def test_can_throws_error_if_address_is_num(self):
+        response = self.app.post(self.base_url, data={'email': 'danila@urfu.ru', 'phone': 987654310,
+                                                      'address': 123456, 'index': 123456,
+                                                      'comment': 'kwarg'})
+        assert response.status_code == 400
+
     def test_can_throws_error_if_address_is_empty(self):
         response = self.app.post(self.base_url, data={'email': 'danila@urfu.ru', 'phone': 9876543210,
                                                       'name': 'Данила', 'index': 123456,
@@ -79,6 +85,12 @@ class TestRegistrationForm(unittest.TestCase):
     def test_can_throws_error_if_index_is_word(self):
         response = self.app.post(self.base_url, data={'email': 'danila@urfu.ru', 'phone': 9876543210,
                                                       'name': 'Данила', 'address': 'Екатеринбург', 'index': 'qwerty',
+                                                      'comment': 'kwarg'})
+        assert response.status_code == 400
+
+    def test_can_throws_error_if_index_is_empty(self):
+        response = self.app.post(self.base_url, data={'email': 'danila@urfu.ru', 'phone': 9876543210,
+                                                      'name': 'Данила', 'address': 'Екатеринбург',
                                                       'comment': 'kwarg'})
         assert response.status_code == 400
 
