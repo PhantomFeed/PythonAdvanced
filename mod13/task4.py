74a9383d5817c92a8c0ac3df63b2cc280bb6e9aa
PhantomFeed <doroh.danil@mail.ru>
2023-04-20 15:03:40 +0500

    mod13

--- mod13/task4.py
+++ mod13/task4.py
@@ -1,18 +1,42 @@
 import sqlite3
 
+sql_request_most_effective = """
+    SELECT salary 
+        FROM table_effective_manager 
+        WHERE name = 'Иван Совин'
+"""
+
+sql_request_salary = """
+    SELECT salary 
+        FROM table_effective_manager 
+        WHERE name = ?
+"""
+
+sql_request_update = """
+    UPDATE table_effective_manager 
+        SET salary = ? 
+        WHERE name = ?
+"""
+
+sql_request_del = """
+    DELETE 
+        FROM table_effective_manager 
+        WHERE name = ?
+"""
+
 
 def ivan_sovin_the_most_effective(cursor: sqlite3.Cursor, name: str) -> None:
-    the_most_effective_employee = cursor.execute("SELECT salary FROM table_effective_manager WHERE name = 'Иван Совин'").fetchone()[0]
-    employee = cursor.execute("SELECT salary FROM table_effective_manager WHERE name = ?", (name,)).fetchone()
+    the_most_effective_employee = cursor.execute(sql_request_most_effective).fetchone()[0]
+    employee = cursor.execute(sql_request_salary, (name,)).fetchone()
     if not employee:
         print(f"Сотрудник {name} не найден в базе данных!")
         return
     salary = int(employee[0] * 1.1)
     if salary <= the_most_effective_employee:
-        cursor.execute("UPDATE table_effective_manager SET salary = ? WHERE name = ?", (salary, name))
+        cursor.execute(sql_request_update, (salary, name))
         print(f"Зарплата сотрудника {name} успешно повышена до {salary} рублей!")
     else:
-        cursor.execute("DELETE FROM table_effective_manager WHERE name = ?", (name,))
+        cursor.execute(sql_request_del, (name,))
         print(f"Сотрудник {name} уволен!")
 
 
@@ -21,6 +45,7 @@ if __name__ == "__main__":
         cursor = db.cursor()
         name = input('Введите имя сотрудника\n>')
         ivan_sovin_the_most_effective(cursor, name)
+
     db.close()
 
 
