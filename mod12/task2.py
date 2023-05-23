42f131ac2ee4031048f81a440b29097b52f6b638
PhantomFeed <doroh.danil@mail.ru>
2023-05-24 01:59:33 +0500

    mod12

--- mod12/task2.py
+++ mod12/task2.py
@@ -12,4 +12,3 @@ def total_memory_usage(root_pid: int) -> float:
     total_memory = float(os.popen(f"ps -o rss= --pid {root_pid} ; ps -o rss= --ppid {root_pid} | awk '{{s+=$1}} END {{print s}}'").read().strip()) / 1024
     return total_memory
 
-
